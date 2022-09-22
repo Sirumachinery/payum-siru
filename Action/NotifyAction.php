e9ccb350c738955c83171ad9190a207083a0c823
Jontsa <joni.halme@sirumobile.com>
2022-09-22 17:43:24 +0300

    add some logging

--- Action/NotifyAction.php
+++ Action/NotifyAction.php
@@ -7,14 +7,18 @@ use Payum\Core\Bridge\Spl\ArrayObject;
 use Payum\Core\Exception\RequestNotSupportedException;
 use Payum\Core\Request\GetHttpRequest;
 use Payum\Core\Request\Notify;
+use Psr\Log\LoggerAwareInterface;
+use Psr\Log\LoggerAwareTrait;
 use Siru\PayumSiru\Action\Api\BaseApiAwareAction;
 use Siru\PayumSiru\Api;
 
 /**
  * @property Api $api
  */
-class NotifyAction extends BaseApiAwareAction
+class NotifyAction extends BaseApiAwareAction implements LoggerAwareInterface
 {
+
+    use LoggerAwareTrait;
 
     /**
      * {@inheritDoc}
@@ -31,14 +35,17 @@ class NotifyAction extends BaseApiAwareAction
         $fields = json_decode($httpRequest->content);
         if (!$fields) {
             // Invalid JSON, maybe this was not a notification from Siru?
+            $this->logger?->warning('Siru NotifyAction called with empty HTTP request body');
             return;
         }
 
         if (!$this->api->isNotificationAuthentic($fields)) {
-            // signature does not match, maybe worth logging?
+            // signature does not match
+            $this->logger?->warning('Siru NotifyAction called with invalid signature in request', $fields);
             return;
         }
 
+        $this->logger?->debug('Siru notification', [$fields['siru_event'], $fields['siru_uuid']]);
         $model['siru_status'] = match ($fields['siru_event']) {
             'success' => 'confirmed',
             'cancel' => 'canceled',
--- Action/SyncAction.php
+++ Action/SyncAction.php
@@ -6,14 +6,18 @@ namespace Siru\PayumSiru\Action;
 use Payum\Core\Bridge\Spl\ArrayObject;
 use Payum\Core\Exception\RequestNotSupportedException;
 use Payum\Core\Request\Sync;
+use Psr\Log\LoggerAwareInterface;
+use Psr\Log\LoggerAwareTrait;
 use Siru\PayumSiru\Action\Api\BaseApiAwareAction;
 use Siru\PayumSiru\Api;
 
 /**
  * @property Api $api
  */
-class SyncAction extends BaseApiAwareAction
+class SyncAction extends BaseApiAwareAction implements LoggerAwareInterface
 {
+
+    use LoggerAwareTrait;
 
     /**
      * {@inheritDoc}
@@ -26,6 +30,7 @@ class SyncAction extends BaseApiAwareAction
 
         $model = ArrayObject::ensureArrayObject($request->getModel());
 
+        $this->logger?->debug('Sync payment status from Siru API', ['uuid' => $model['siru_uuid']]);
         $status = $this->api->checkStatus($model['siru_uuid']);
         $model['siru_status'] = $status['status'];
     }
